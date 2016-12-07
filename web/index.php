708a4efcb8b82e0bf3dbffc98ba27722fe561ce8
Florian Adonis <tanohzana@gmail.com>
2016-12-07 17:52:17 +0100

    bug fixing

--- web/index.php
+++ web/index.php
@@ -24,41 +24,45 @@ $app->post('/webhook', function(Request $request) use($app) {
 	//$req=json_encode($request);
 	//$req=json_decode($request, true);
 
-	$req = $request->request->get('result');
+	$post->bindRequest($request);
+    // data is an array with "name", "email", and "message" keys
+    $data = $post->getData();
+
+	// $req = $request->request->get('result');
 
 	//echo "Request : ".json_encode($req);
 
-	//foreach($req as $key => $value){
-		// if($req['action'] != "find.name")
-		// 	$res="empty";
-		// else{
-			// $result=$req['result'];
-			// $parameters=$result['parameters'];
-			// $surname=$parameters['names'];
+	foreach($data as $key => $value){
+		 if($value['action'] != "find.name")
+			$speech="Je ne sais pas.";
+		else{
+			$result=$data['result'];
+			$parameters=$result['parameters'];
+			$surname=$parameters['names'];
 
-			// $users=array(
-			// 	'Florian'=>'Adonis',
-			// 	'Emna'=>'Bouzouita',
-			// 	'Alex'=>'Guilngar'
-			// );
+			$users=array(
+				'Florian'=>'Adonis',
+				'Emna'=>'Bouzouita',
+				'Alex'=>'Guilngar'
+			);
 
 			$users=json_encode($users);
 
-			// $speech="The name of ".$surname." is ".$users[$surname].".";
+			$speech="The name of ".$surname." is ".$users[$surname].".";
 
-			$speech="The name of Florian is Adonis.";
+			// $speech="The name of Florian is Adonis.";
 
-			//echo "Response: ".$speech;
+			
+		}
+	}
 
-			$res=array(
-				"speech"=> $speech, 
-				"displayText"=> $speech, 
-				// "data"=> [], 
-				// "contextOut"=> [],
-				"source"=> "apiai-test-php"
-		    );
-		// }
-	//}
+	$res=array(
+		"speech"=> $speech, 
+		"displayText"=> $speech, 
+		// "data"=> [], 
+		// "contextOut"=> [],
+		"source"=> "apiai-test-php"
+    );
 
 	//$res=json_encode($res);
 
