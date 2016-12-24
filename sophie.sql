c6bfde264b57add0a266dc54c9c962bd12f36e59
Florian Adonis <tanohzana@gmail.com>
2016-12-24 03:17:50 +0100

    serial primary keys

--- sophie.sql
+++ sophie.sql
@@ -1,61 +1,55 @@
 CREATE TABLE medecins(
-	id 		INT,
+	id 		INT serial primary key,
 	nom VARCHAR(40),
 	prenom VARCHAR(30),
 	email  VARCHAR(30),
-	telephone INT NOT NULL,
-	PRIMARY KEY(id)
+	telephone INT NOT NULL
 );
  
 CREATE TABLE utilisateurs(
-	id 		INT,
+	id 		INT serial primary key,
 	nom   VARCHAR(30),
 	email  VARCHAR(30),
 	prenom  VARCHAR(30),
 	date_de_naissance  DATE,
-	adresse   VARCHAR(40),
-	PRIMARY KEY(id)
+	adresse   VARCHAR(40)
 );
 
 CREATE  TABLE entourage(
-	id 		INT,
+	id 		INT serial primary key,
 	nom    VARCHAR(30),
 	prenom  VARCHAR(30),
 	lien_utilisateur  VARCHAR(30),
 	id_utilisateur   INT,
 	lien    VARCHAR(30),
 	lien_photo VARCHAR(30),
-	PRIMARY KEY(id),
 	FOREIGN KEY(id_utilisateur) REFERENCES utilisateurs(id)
 );
 
 CREATE TABLE rdv(
-	id 				INT,
+	id 				INT serial primary key,
 	label 			VARCHAR(40),
 	lieu 			VARCHAR(20),
 	date_rdv 		DATE,
 	time_rdv		TIME,
 	id_personne   	INT,
-	PRIMARY KEY(id),
 	FOREIGN KEY(id_personne) REFERENCES entourage(id)
 );
  
 CREATE TABLE lieux(
-	id 		INT,
+	id 		INT serial primary key,
 	nom  VARCHAR(30),
 	pays  VARCHAR(30),
 	id_utilisateur  INT,
 	lien_photo  VARCHAR(30),
-	PRIMARY KEY(id),
 	FOREIGN KEY(id_utilisateur) REFERENCES utilisateurs(id)
 );
  
 CREATE  TABLE taches(
-	id 		INT,
+	id 		INT serial primary key,
 	nom    VARCHAR(30),
 	description  VARCHAR(30),
 	date_taches    DATE,
 	id_utilisateur   INT,
-	PRIMARY KEY(id),
 	FOREIGN KEY(id_utilisateur) REFERENCES utilisateurs(id)
 );
