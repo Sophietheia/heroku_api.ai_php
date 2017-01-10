1d4bf9e20925a9d3543d19f4a8cfabcca678e73d
Florian Adonis <tanohzana@gmail.com>
2017-01-10 19:27:22 +0100

    bug fix

--- web/index.php
+++ web/index.php
@@ -246,7 +246,7 @@ $app->post('/webhook', function(Request $request) use($app) {
 	else if($result['action'] == "avec.qui.meeting"){
 		$today=date("Y-m-d");
 
-		$query = pg_prepare($db, "avec_meeting", "SELECT rdv.label AS label, entourage.nom AS nom, entourage.prenom AS prenom FROM rdv, entourage WHERE rdv.id_utilisateur=$1 AND rdv.date_rdv>='$today' AND rdv.id_personne=entourage.id GROUP BY label, id HAVING date_rdv=MIN(date_rdv);
+		$query = pg_prepare($db, "avec_meeting", "SELECT rdv.label AS label, entourage.nom AS nom, entourage.prenom AS prenom FROM rdv, entourage WHERE rdv.id_utilisateur=$1 AND rdv.date_rdv>='$today' AND rdv.id_personne=entourage.id GROUP BY rdv.label, rdv.id HAVING rdv.date_rdv=MIN(rdv.date_rdv);
 ");
 		
 		$result = pg_execute($db, "avec_meeting", array(ID));
