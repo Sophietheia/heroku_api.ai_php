227e028551ecb8d08cae0a6253e5402e54e306d9
Emna Bouzouita <ebouzouita@gmail.com>
2017-01-10 08:44:07 -0800

    selection meeting avec min date

--- web/index.php
+++ web/index.php
@@ -199,14 +199,14 @@ $app->post('/webhook', function(Request $request) use($app) {
 	else if($result['action'] == "location.meeting"){
 		
 
-		$query = pg_prepare($db, "location_meeting", "SELECT lieu FROM rdv WHERE id_utilisateur=$1");
+		$query = pg_prepare($db, "location_meeting", "SELECT lieu FROM rdv WHERE id_utilisateur=$1" AND date_rdv=MIN(date_rdv);
 		
 		$result = pg_execute($db, "location_meeting", array(ID));
 
 		$arr = pg_fetch_array ($result, 0, PGSQL_NUM);
 
 		$location = $arr[0];
-		//------------------------------------------------------
+	
 
 		if($location)
 			$speech=" Your next meeting is at ".$location.".";
