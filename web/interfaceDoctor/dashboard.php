c2a4a01e15b12fef3983781ab526794cecefac1a
tanohzana <tanohzana@gmail.com>
2017-02-28 10:15:17 +0100

    adding interfaceDoctor folder

---
+++ web/interfaceDoctor/dashboard.php
@@ -0,0 +1,34 @@
+<?php
+
+$app->get('/dashboardDoctor', function() use($app){
+  $app['idDoc'] = IDDOC;
+  $app['users'] = json_decode(getUsersList(), true);
+  $app['test2'] = 'test 2 vide';
+  return $app['twig']->render('dashboard.twig');
+});
+
+$app->post('/dashboardDoctor', function(Request $request) use($app){
+  $app['idDoc'] = IDDOC;
+  $app['users'] = json_decode(getUsersList(), true);
+
+  // $form = $app['form.factory']->createBuilder(FormType::class, $data)
+  //       ->add('idDoc')
+  //       ->add('idPatient')
+  //       ->add('label')
+  //       ->add('date')
+  //       ->add('time')
+  //       ->add('location')
+  //       ->getForm();
+  //
+  // $form->handleRequest($request);
+  //
+  // if ($form->isSubmitted() && $form->isValid()) {
+  //     $app['test2'] = $form->getData();
+  // }
+
+  $data = $request->request->all();//get('form');
+
+  $app['test2'] = $data;
+
+  return $app['twig']->render('dashboard.twig');
+});
