c36576c78a05939c6cfda16d6c49c57edda691c8
tanohzana <tanohzana@gmail.com>
2017-02-28 10:53:47 +0100

    adding rdv to DB

---
+++ web/interfaceDoctor/dashboard.controller.php
@@ -0,0 +1,28 @@
+<?php
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+require('dashboard.model.php');
+
+$app->get('/dashboardDoctor', function() use($app){
+  $app['idDoc'] = IDDOC;
+  $app['users'] = json_decode(getUsersList(), true);
+  $app['test2'] = 'test 2 vide';
+  return $app['twig']->render('dashboard.twig');
+});
+
+$app->post('/dashboardDoctor', function(Request $request) use($app){
+  $app['idDoc'] = IDDOC;
+  $app['users'] = json_decode(getUsersList(), true);
+
+  $newRdv['idDoc'] = $request->get('idDoc');
+  $newRdv['idPatient'] = $request->get('idPatient');
+  $newRdv['label'] = $request->get('label');
+  $newRdv['date'] = $request->get('date');
+  $newRdv['time'] = $request->get('time');
+  $newRdv['label'] = $request->get('location');
+
+  newRdv($newRdv);
+
+  return $app['twig']->render('dashboard.twig');
+});
