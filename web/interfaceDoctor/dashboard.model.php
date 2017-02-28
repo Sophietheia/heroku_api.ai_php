696f0853cf5c54c7f6b07bbe8010ffe3d5ceb1c2
tanohzana <tanohzana@gmail.com>
2017-02-28 11:04:35 +0100

    bug fix

--- web/interfaceDoctor/dashboard.model.php
+++ web/interfaceDoctor/dashboard.model.php
@@ -4,5 +4,5 @@ function newRdv($rdv){
   $db = db_connect();
 
   $query = pg_prepare($db, "add_rdv", "INSERT INTO meetings(label, location, date_meeting, time_meeting, id_user, id_person) VALUES($1, $2, $3, $4, $5, $6)");
-  pg_execute($db, "add_person", array($rdv['label'], $rdv['location'], $rdv['date_meeting'], $rdv['time_meeting'], $rdv['idPatient'], $rdv['idDoc']));
+  pg_execute($db, "add_rdv", array($rdv['label'], $rdv['location'], $rdv['date_meeting'], $rdv['time_meeting'], $rdv['idPatient'], $rdv['idDoc']));
 }
