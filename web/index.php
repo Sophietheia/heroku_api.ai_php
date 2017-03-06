24b9abcc4396942c1133358e46fca3483430f74b
Emna Bouzouita <ebouzouita@gmail.com>
2017-03-06 05:00:46 -0800

    requete pour stade maladie4

--- web/index.php
+++ web/index.php
@@ -84,7 +84,7 @@ $app->post('/memory', function(Request $request) use($app){
 
   $response = array();
     //$response['stade'] = "1";
-  $query = pg_prepare($db, "get_stade", "SELECT stade FROM users WHERE id=1;");
+  $query = pg_prepare($db, "get_stade", "SELECT stade FROM users WHERE id=$1;");
   $response['stade']  = pg_execute($db, "get_stade", array(ID));
   
 
