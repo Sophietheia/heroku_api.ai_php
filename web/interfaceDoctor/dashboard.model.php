0fbb25d9e0585b2dfcedc47c6c3c9605dd6db404
tanohzana <tanohzana@gmail.com>
2017-03-09 10:51:02 +0100

    status to true on adding user

--- web/interfaceDoctor/dashboard.model.php
+++ web/interfaceDoctor/dashboard.model.php
@@ -58,7 +58,7 @@ function getUsersListByDoctor($iddoc){
 function addUser($newUser){
   $db = db_connect();
 
-  $query = pg_prepare($db, "insert_users", "INSERT INTO users(iddoctor,name,surname,username,password,phonenumber,address) VALUES($1,$2,$3,$4,$5,$6,$7);");
+  $query = pg_prepare($db, "insert_users", "INSERT INTO users(iddoctor,name,surname,username,password,phonenumber,address,status) VALUES($1,$2,$3,$4,$5,$6,$7,$8);");
 
-  pg_execute($db, "insert_users", array($newUser['iddoctor'],$newUser['name'], $newUser['surname'], $newUser['login'], sha1($newUser['password']), $newUser['phonenumber'], $newUser['address']));
+  pg_execute($db, "insert_users", array($newUser['iddoctor'],$newUser['name'], $newUser['surname'], $newUser['login'], sha1($newUser['password']), $newUser['phonenumber'], $newUser['address'],true));
 }
