f6c91ac7c35ed0b197fc70bb43723c127dc837fd
Emna Bouzouita <ebouzouita@gmail.com>
2017-03-09 12:12:01 -0800

    try bug fix game

--- web/index.php
+++ web/index.php
@@ -104,11 +104,11 @@ $app->post('/memory', function(Request $request) use($app){
     $response['stade'] = "1";
   /*$query = pg_prepare($db, "get_stade", "SELECT stade FROM users WHERE id=$1;");
     $result= pg_execute($db, "get_stade", array(ID));
-    $response=pg_fetch_row($result);
+    $response=pg_fetch_row($result);*/
     if($response[0]==1)
     	$response['stade']=true;
  	if($response[0]==2)
-    	$response['stade']=false;*/
+    	$response['stade']=false;
 
   return json_encode($response);
 });
