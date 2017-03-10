942af455f39601ca6f400abb3fb524d43e3c7b74
tanohzana <tanohzana@gmail.com>
2017-03-10 13:04:50 +0100

    bug fix

--- web/interfaceDoctor/dashboard.model.php
+++ web/interfaceDoctor/dashboard.model.php
@@ -67,9 +67,9 @@ function getUsersListByDoctor($iddoc){
 function addUser($newUser){
   $db = db_connect();
 
-  $query = pg_prepare($db, "insert_users", "INSERT INTO users(iddoctor,name,surname,username,password,phonenumber,address,status) VALUES($1,$2,$3,$4,$5,$6,$7,$8);");
+  $query = pg_prepare($db, "insert_users", "INSERT INTO users(iddoctor,name,surname,username,password,phonenumber,address,status,alertzone) VALUES($1,$2,$3,$4,$5,$6,$7,$8);");
 
-  pg_execute($db, "insert_users", array($newUser['iddoctor'],$newUser['name'], $newUser['surname'], $newUser['login'], sha1($newUser['password']), $newUser['phonenumber'], $newUser['address'],true));
+  pg_execute($db, "insert_users", array($newUser['iddoctor'],$newUser['name'], $newUser['surname'], $newUser['login'], sha1($newUser['password']), $newUser['phonenumber'], $newUser['address'],true,true));
 }
 
 function checkLoginDoctor($username, $password){
