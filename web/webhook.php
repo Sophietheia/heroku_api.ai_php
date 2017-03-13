4bce9c0bc86361361fe6061c1d843b212d11b162
tanohzana <tanohzana@gmail.com>
2017-03-13 10:51:43 +0100

    trying to get the sessionId

--- web/webhook.php
+++ web/webhook.php
@@ -10,6 +10,10 @@ $app->post('/webhook', function(Request $request) use($app) {
 
 	$result = $request->request->get('result');
 
+	$sessionId = $request->request->get('sessionId');
+
+
+
 
 /////if user wants to change name
 
