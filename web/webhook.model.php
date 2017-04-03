783b4ed3c9d006a1aae792bb9b0440efaac77a41
tanohzana <tanohzana@gmail.com>
2017-04-03 12:30:54 +0200

    bug fix

--- web/webhook.model.php
+++ web/webhook.model.php
@@ -1,35 +1,37 @@
 <?php
   function get_name_from_relation($surname, $id){
+    $db = db_connect();
+
     $query = pg_prepare($db, "surname_name", "SELECT name FROM relations WHERE surname=$1 AND id_user=$2");
 		$result = pg_execute($db, "surname_name", array($surname, $id));
 
-		$name = pg_fetch_result($result);
+		$name = pg_fetch_row($result);
 
-		return $name;
+		return $name[0];
   }
 
   function get_id_person($user_id){
     $db = db_connect();
-
-    logPerso("getID user_id:",$user_id);
 
     $query = pg_prepare($db, "id", "SELECT id FROM users WHERE req_id=$1");
 		$result = pg_execute($db, "id", array($user_id));
 
     $id = pg_fetch_row($result);
 
-    logPerso("getID id:",$id[0]);
-
     return $id[0];
   }
 
   function add_rdv($label, $location, $date_meeting, $time, $id, $id_perso){
+    $db = db_connect();
+
     $query = "INSERT INTO meetings(label, location, date_meeting, time_meeting, id_user, id_person) VALUES('$label', '$location', '$date_meeting', '$time', '".$id."', '$id_perso');";
 
     pg_query($db, $query);
   }
 
   function location_meeting($id){
+    $db = db_connect();
+
     $today=date("Y-m-d");
 		$query = pg_prepare($db, "location_meeting", "SELECT label, location FROM meetings WHERE id_user=$1 AND date_meeting>=$2 GROUP BY label, id HAVING date_meeting=MIN(date_meeting);");
 
@@ -41,6 +43,8 @@
   }
 
   function date_meeting($id){
+    $db = db_connect();
+
     $today=date("Y-m-d");
 		$query = pg_prepare($db, "date_meeting", "SELECT label, date_meeting FROM meetings WHERE id_user=$1 AND date_meeting>=$2 GROUP BY label, id HAVING date_meeting=MIN(date_meeting);");
 
@@ -52,6 +56,8 @@
   }
 
   function with_who_meeting($id){
+    $db = db_connect();
+
     $today=date("Y-m-d");
     $query = pg_prepare($db, "with_meeting", "SELECT meetings.label AS label, relations.name AS name, relations.surname AS surname FROM meetings, relations WHERE meetings.id_user=$1 AND meetings.date_meeting>='$today' AND meetings.id_person=relations.id GROUP BY meetings.label, meetings.id, relations.id HAVING meetings.date_meeting=MIN(meetings.date_meeting);
 ");
