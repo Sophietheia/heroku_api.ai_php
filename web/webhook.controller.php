17b0d1ac7f06f6b562ad694ec83a8724dba72de2
tanohzana <tanohzana@gmail.com>
2017-04-03 12:04:27 +0200

    bug fix

--- web/webhook.controller.php
+++ web/webhook.controller.php
@@ -2,6 +2,7 @@
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
+require('functions.php');
 require('webhook.model.php');
 
 $app->post('/webhook', function(Request $request) use($app) {
