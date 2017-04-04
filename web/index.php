67f3237d7ce8541ebaca9e09207d5a2cfd62efbe
tanohzana <tanohzana@gmail.com>
2017-04-04 15:25:10 +0200

    bug fix

--- web/index.php
+++ web/index.php
@@ -139,7 +139,7 @@ $app->post('/reminders', function(Request $request) use($app){
     $remind["date_task"] = $row["date_meeting"];
     $remind["time_task"] = $row["time_meeting"];
     if($row["surname"]=="")
-      $remind["surname"] = "docteur";
+      $remind["surname"] = "doctor";
     else
       $remind["surname"] = $row["surname"];
 
